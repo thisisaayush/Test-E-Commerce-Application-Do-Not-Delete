6a638bf16a0da7cdfc4b18e054ca975e539f098d
thisisaayush <thakurikarthik@gmail.com>
2023-11-28 20:06:01 -0600

    started the unitofwork

--- obj/Debug/net8.0/E-commerce-Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-commerce-Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e36db9e7e8b3d0de53ae88d01ce33fe72dd471c2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e952dd1fa6dadabd6554fd1cefde43634f08367")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
