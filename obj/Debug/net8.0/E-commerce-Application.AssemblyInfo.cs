5f8b9a64f368a94964d429d654e234e0c4083993
thisisaayush <thakurikarthik@gmail.com>
2023-12-02 20:15:36 -0600

    added unit of work - product

--- obj/Debug/net8.0/E-commerce-Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-commerce-Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+79ccb63c03e63f60bea7ef63d7273422f94a59d3")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
