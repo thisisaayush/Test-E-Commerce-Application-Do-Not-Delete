37ee3fa548c65653b56e3da24a6115eddcc9ede7
thisisaayush <thakurikarthik@gmail.com>
2023-12-03 21:50:45 -0600

    migration changes for product model is updated

--- obj/Debug/net8.0/E-commerce-Application.AssemblyInfo.cs
+++ obj/Debug/net8.0/E-commerce-Application.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bb5b731daac9a71d3fff5114f00cf90e92571d7a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("E-commerce-Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
